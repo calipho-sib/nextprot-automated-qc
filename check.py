92bde824a0b26a6e68cf81ead65cd617f1e39a0e
Daniel Teixeira <ddtxra@gmail.com>
2018-06-08 13:21:54 +0200

    addding HTML tags macro

--- check.py
+++ check.py
@@ -1,4 +1,5 @@
 import re, csv, urllib2, glob, sys, argparse
+import datetime
 from termcolor import cprint
 
 success = True
@@ -19,6 +20,7 @@ class URLTest:
         self.note = note
 
 def buildUrl(relativeUrl):
+    #If if comes from failures it is already with http
     global baseUrl
     return baseUrl + relativeUrl + "?_escaped_fragment_="
 
@@ -28,13 +30,15 @@ def checkForEachUrl(urlTest, params):
 	url = buildUrl(urlTest.url)
     else:
         url = buildUrl(urlTest.url)
-    #print("Requesting " + url + " ...")
+
     #If the url requested is the same, just return the last content
     if(lastUrlRequested != url):
+        print("\tHTTP(S)_REQUEST: ... requesting content of " + url + " ...")
         content = urllib2.urlopen(url).read()
         lastUrlRequested = url
         lastContent = content
     else:
+        print("\tCACHED: Taking content of " + url + " from cache")
         content = lastContent
 
     if(urlTest.expression == "containsText"):
@@ -60,23 +64,38 @@ def readFile(file):
             lineCount = lineCount + 1
             if((len(row) > 0) and (not row[0].startswith("#"))):
                 try:
-                    urls.append(URLTest(row[0], row[1], row[2], row[3]))
+                    urls.append(URLTest(row[0], row[1], str(row[2]).replace("<--HTMLTAGS-->", "((.|\\n|\\r){1,500})"), row[3]))
                 except:
                     print("Failed to read line " + str(lineCount) + " for " + str(file) + " found " + str(len(row)) + " columns. Minimum is 3. Row is: " + str(row));
                     sys.exit(1);
                     raise;
     return urls;
 
+def saveErrors(errors):
+    print("Writing remaining errors in errors.tsv")
+    f = open("errors.tsv","w+")
+    f.write("relativeUrl\texpression\tvalue\tcomment\n")
+    for error in errors:
+        f.write(error.urlTest.url + "\t" + error.urlTest.expression + "\t" + error.urlTest.value + "\t" + error.urlTest.note + "\n")
+    f.close() 
+
 def testFile(file, params):
     urlTestResults = []
     urlTests = readFile(file)
+    urlTests = sorted(urlTests, key=lambda ut: ut.url)
+    count = 1
     for urlTest in urlTests:
-        print("Testing " + urlTest.url + "\t" +  urlTest.expression + "\t" + urlTest.value + "\n\t" + urlTest.note + "")
+        start = datetime.datetime.now()
+        print(str(count) + "/" + str(len(urlTests)) + " Testing " + urlTest.url + "\t" +  urlTest.expression + "\t" + urlTest.value + "\n\tSpec: " + urlTest.note + "")
         result = checkForEachUrl(urlTest, params)
+        end = datetime.datetime.now()
+        diff = end - start
+        elapsed_ms = (diff.days * 86400000) + (diff.seconds * 1000) + (diff.microseconds / 1000)
         if(result):
-            cprint("\tOK", 'green')
+            cprint("\t%s in %d ms" %("OK", elapsed_ms), 'green')
         else:
-            cprint("\tERROR", 'red')
+            cprint("\t%s in %d ms" %("ERROR", elapsed_ms), 'red')
+        count = count + 1
         urlTestResults.append(URLTestResult(urlTest, result))
     errors = filter(lambda x: x.result == False, urlTestResults)
     if(len(errors) > 0):
@@ -85,9 +104,9 @@ def testFile(file, params):
         print (str(len(errors)) + "/" + str(len(urlTestResults)) + " tests failed in " + file)
         for error in errors:
             print ("\tERROR: " + buildUrl(error.urlTest.url) + " " + error.urlTest.expression + " " + error.urlTest.value)
+        saveErrors(errors)
     else:
         print (str(len(urlTestResults)) + " tests passed in " + file)
-
 
 if(not success):
     print "JOB FAILED!"
@@ -101,15 +120,19 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    print("\n########### neXtProt Automated QC configuration ###########################################") 
     if(args.baseUrl):
         baseUrl = args.baseUrl;
-        print("Overriding base url to " + baseUrl) 
+        print("## --baseUrl: Overriding base url with: " + baseUrl) 
     else:
-        print("Default base url is: " + baseUrl + ", can be overriden by passing a parameter: python check.py --baseUrl http://alpha-search.nextprot.org")
+        print("## --baseUrl: Default base url is: " + baseUrl + ", can be overriden by passing a parameter: python check.py --baseUrl http://alpha-search.nextprot.org")
 
     if(args.file):
+        print("## --file: Using file " + args.file) 
+        print("###########################################################################################\n") 
         testFile(args.file, args)
     else:
-        print ("Checking for all files in test-specs folder")
+        print("## --file: Using all files in folder test-specs") 
+        print("###########################################################################################\n") 
         for file in glob.glob("test-specs/*.tsv"):
             testFile(file, args)
